ce50ecf966a2f3838c8ac4d8519b8ab231a9dd25
Vatana Rianti Aldefi <Vatana Rianti Aldefi>
2017-03-21 21:00:56 +0700

    Alhamdulillah

--- DataLatihan/answer1.py
+++ DataLatihan/answer1.py
@@ -1,5 +1,5 @@
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
+data1 = "Data1.txt"
+data2 = "Data2.txt"
 
 def readData(data1):
 	x = []
@@ -7,3 +7,14 @@ def readData(data1):
 	    for line in data :
 		    x = line.split()
 	return x
+
+x = readData(data1)
+array = []
+for each in x:
+	if (each == "I" or each == "you" or each == "Then" or each == "and") :
+		array.append('*'*len(each))
+	else :
+		array.append(each)
+
+data = " ".join(array)
+print(data)
--- DataLatihan/answer2.py
+++ DataLatihan/answer2.py
@@ -1,9 +1,19 @@
-data1 = "Data_1.txt"
-data2 = "Data_2.txt"
+data1 = "Data1.txt"
+data2 = "Data2.txt"
 
 def readData(data1):
 	x = []
 	with open(data1) as data :
 	    for line in data :
 		    x = line.split()
-return x
+	return x
+
+x = readData(data1)
+y = readData(data2)
+array = []
+for eachx in x:
+	for eachy in y:
+		if (eachx == eachy) :
+			if (eachx not in array) :
+				array.append(eachx)
+print(array)
